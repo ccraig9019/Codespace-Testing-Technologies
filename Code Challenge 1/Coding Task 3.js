8a12c30f96df2338faff95bddfec25352a93816c
Calum Craig <calumjamescraig@gmail.com>
2024-09-15 14:10:35 +0100

    Added comment to Coding Task 3.js

--- Code Challenge 1/Coding Task 3.js
+++ Code Challenge 1/Coding Task 3.js
@@ -7,6 +7,7 @@
     let costs = arrayInput.map(item => item.cost); //assigning costs to a new array
     //console.log(costs); //for debugging
     let largestCost = Math.max(...costs); //finding the largest item within the new costs array and assigning it to a variable
+    //Note on above line: the Math.max() method can't operate on arrays. The ... is the spread method, which separates the elements of an array into a list of arguments
     //console.log(largestCost); //for debugging
     let maxIndex = costs.indexOf(largestCost); //finding the index number of the new costs array and assigning it to a variable
     delete arrayInput[maxIndex].cost; //deleting the cost property from the object with the highest cost
